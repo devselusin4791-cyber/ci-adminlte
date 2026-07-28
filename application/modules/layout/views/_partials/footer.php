82247c6fce121a1fa343f00c3c615f769f49de97
username <user@mail.com>
2026-07-28 22:11:28 +0700

    .

--- application/modules/layout/views/_partials/footer.php
+++ application/modules/layout/views/_partials/footer.php
@@ -1,7 +1,7 @@
 <!--begin::Footer-->
 <footer class="app-footer">
   <!--begin::To the end-->
-  <div class="float-end d-none d-sm-inline">v1.0.0.0</div>
+  <div class="float-end d-none d-sm-inline">v1.0</div>
   <!--end::To the end-->
   <!--begin::Copyright-->
   <strong>
