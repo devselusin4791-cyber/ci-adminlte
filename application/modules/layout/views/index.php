82819630dd4053b64cc0a0a7fdcad968cdd3d878
selarassolusindo-apps <dodo.selarassolusindo@gmail.com>
2026-07-29 10:15:36 +0700

    .

--- application/modules/layout/views/index.php
+++ application/modules/layout/views/index.php
@@ -7,6 +7,7 @@
       <!--begin::App Wrapper-->
       <div class="app-wrapper">
           <?php $this->load->view('_partials/navbar'); // Panggil potongan navbar ?>
+          <?php $this->load->view('_partials/sidebar'); // Panggil potongan navbar ?>
 
           <!--begin::App Main-->
           <main class="app-main">
